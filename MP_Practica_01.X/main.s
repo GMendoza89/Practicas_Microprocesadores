fd6003416aadabf7293795278b29e8663f69777f
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2022-06-15 13:22:34 -0500

    Se corige el codigo principal usado para el uso del PIC-AS incluido en el compilador xc8 de Microchip

---
+++ MP_Practica_01.X/main.s
@@ -0,0 +1,55 @@
+
+ ;PIC18F4550 Configuration Bit Settings
+
+; Assembly source line config statements
+;PROCESSOR 18F4550
+
+; PIC18F4550 Configuration Bit Settings
+
+; Assembly source line config statements
+
+#include <xc.inc>
+; CONFIG1H
+  CONFIG  FOSC = HS             ; Oscillator Selection bits (HS oscillator (HS))
+; CONFIG2L
+  CONFIG  PWRT = ON            ; Power-up Timer Enable bit (PWRT disabled)
+; CONFIG2H
+  CONFIG  WDT = OFF             ; Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+; CONFIG3H
+  CONFIG  PBADEN = OFF          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+  CONFIG  MCLRE = ON            ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+; CONFIG4L
+  CONFIG  LVP = OFF             ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+
+
+PSECT DATAS
+ TMP:
+    DS 1; TMP -> i BYTE
+
+PSECT _main, CLASS = CODE, reloc=2
+ 
+
+ main:
+    goto INICIO
+    
+  PSECT CODE
+  INICIO:
+    counterOne EQU 020h
+    counterTwo EQU 021h
+    movlw 00001111B
+    movwf ADCON1
+    clrf TRISD
+LOOP:    
+    movlw 00fh
+    decf LATD
+    goto delay
+delay:
+    decfsz counterOne
+    goto delay
+    decfsz counterTwo
+    goto delay
+    goto LOOP
+    
+    
+    END main
+    

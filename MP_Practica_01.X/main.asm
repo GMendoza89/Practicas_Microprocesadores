473be58ff5102d5f3b191bbbf4ca317a5b52c51f
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2022-09-26 12:49:59 -0500

    Se corrigen los datos de la practica 1, se crea el código con compactibilidad al compilador PIC-AS integrado con el XC8

---
+++ MP_Practica_01.X/main.asm
@@ -0,0 +1,44 @@
+
+;PROCESSOR 18F4550
+
+; PIC18F4550 Configuration Bit Settings
+
+; Assembly source line config statements
+
+#include <xc.inc>
+; CONFIG1H
+  CONFIG  FOSC = HS             ; Oscillator Selection bits (HS oscillator (HS))
+; CONFIG2L
+  CONFIG  PWRT = ON            ; Power-up Timer Enable bit (PWRT disabled)
+; CONFIG2H
+  CONFIG  WDT = OFF             ; Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+; CONFIG3H
+  CONFIG  PBADEN = OFF          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+  CONFIG  MCLRE = ON            ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+; CONFIG4L
+  CONFIG  LVP = OFF             ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+
+PSECT _main, CLASS = CODE, reloc=2
+ 
+ _main:
+    goto STARTMAIN
+    
+  PSECT CODE
+STARTMAIN:
+    counterOne EQU 020h
+    counterTwo EQU 021h
+    CLRF PORTD
+    CLRF LATD
+    clrf TRISD
+LOOP:    
+    decf LATD
+    goto delay
+delay:
+    decfsz counterOne
+    goto delay
+    decfsz counterTwo
+    goto delay
+    goto LOOP
+     
+END _main
+    
